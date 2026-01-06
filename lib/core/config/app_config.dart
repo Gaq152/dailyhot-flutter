b4ff2f8e3b3d1f74afa0705691d990501f3480d3
gonganqi <gonganqi@gddi.com.cn>
2026-01-06 15:00:01 +0800

    chore: 发布 v1.3.4

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -19,7 +19,7 @@ class AppConfig {
 
   /// 应用信息
   static const String appName = '每日热点';
-  static const String appVersion = '1.3.3';
+  static const String appVersion = '1.3.4';
   static const String appDescription = '每日热点聚合';
 
   /// 网络配置
